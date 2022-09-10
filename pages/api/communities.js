1ef0b1441d49e916a9689887281e67c4b9a55934
magiobus <aalbertobus@gmail.com>
2022-09-10 01:45:32 -0600

    Showing only verified

--- pages/api/communities.js
+++ pages/api/communities.js
@@ -6,7 +6,13 @@ export default async function handler(req, res) {
     try {
       const records = await table.select({}).firstPage();
       const minfiedRecords = minifyItems(records);
-      res.status(200).json({ communities: minfiedRecords });
+      //return only the ones that are approved
+      console.log("minfiedRecords =>", minfiedRecords);
+      const approvedCommunities = minfiedRecords.filter(
+        (item) => item.fields["Verificación"] === true
+      );
+
+      res.status(200).json({ communities: approvedCommunities });
     } catch (error) {
       console.error(error);
       res.status(500).json({ msg: "Something went wrong getting records! 😕" });
